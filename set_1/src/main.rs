b7a787560fdb67d69e29a7b62e3973dc6cd8d7c5
huynguyenau <huynguyenau@gmail.com>
2020-04-08 20:19:05 +1000

    Cleaup up. Need to add comments!

--- set_1/src/main.rs
+++ set_1/src/main.rs
@@ -1,16 +1,22 @@
 fn main() {
-	/*
-	let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-	let base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-	let result = hex_2_base64(hex);
+	test_hex_2_base64();
+	test_fixed_xor();
+}
 
-	assert_eq!(result, base64, "We are testing if converting hex to base64 works.");
-	*/
+fn test_hex_2_base64() {
+	let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+	let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
+	assert_eq!(hex_2_base64(input), expected, "Testing Fixed XOR in Set 1 Challenge 1.");
+}
+
+fn test_fixed_xor() {
 	let left = "1c0111001f010100061a024b53535009181c";
 	let right = "686974207468652062756c6c277320657965";
+	let expected = "746865206b696420646f6e277420706c6179";
+	let result = fixed_xor(left, right);
 
-	println!("{}", fixed_xor(left, right));		
+	assert_eq!(fixed_xor(left, right), expected, "Testing Fixed XOR in Set 1 Challenge 2.");
 }
 
 fn hex_2_base64(value: &str) -> String {
